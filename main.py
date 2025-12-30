229cac5dfa510ea2c24ebd49b2d92e8446f72b09
VextorHorizon <Coptersrichanth@gmail.com>
2025-12-30 15:10:30 +0700

    changes again

--- main.py
+++ main.py
@@ -15,8 +15,18 @@ html_content = BeautifulSoup(url, "html.parser") #กำหนดกฎที่
 #หา tags img ในตัวของ content ที่ได้มา
 imgs_in_targetweb = html_content.find_all('img') #ได้ list ขอ ที่แปะแท็ค img ทั้งหมดในกล่อง html_content
 
+png = []
+jpg = []
+webp = []
 
-                
+for img in imgs_in_targetweb: #loop วนดูใน list imgs_in_targetweb ก้คือ img ไล่ไปทีละตัวๆใน imgs_in_targetweb เลย
+    img_url = img['src'] #แยกทุกอย่างในแท็ก <img> ออก ให้เหลือแต่ไส้ในที่เราต้องการ ซึ่งคือ src มันมี image url อยู่
+    if 'png' in img_url: png.append(img_url) #แยก png เข้า list กล่องชื่อ png
+    elif 'jpg' in img_url: jpg.append(img_url) #แยก jpg เข้า list กล่องชื่อ jpg
+    elif 'webp' in img_url: webp.append(img_url) #แยก webp เข้า list กล่องชื่อ webp
+    else: pass
+
+
 
 
 
